e1c789837b7782b9c398e036d4ca0d8c59d2f102
Hakanasa <louisjonathan29@gmail.com>
2021-04-03 14:37:23 +0700

    Profile View, Edit Profile View, Option Menu Edit View Layout

--- app/src/main/java/umn/ac/id/tugasakhir_android/ViewProfileActivity.java
+++ app/src/main/java/umn/ac/id/tugasakhir_android/ViewProfileActivity.java
@@ -2,9 +2,12 @@ package umn.ac.id.tugasakhir_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ViewProfileActivity extends AppCompatActivity {
 
@@ -16,5 +19,24 @@ public class ViewProfileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Profile");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.edit_profile, menu);
+        return true;
+    }
+
+    //Action Dari Option List
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.profilein:
+                Intent intent = new Intent(getApplicationContext(), EditProfileActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
