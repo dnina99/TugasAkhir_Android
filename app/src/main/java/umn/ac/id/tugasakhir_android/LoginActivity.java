fc8523ad82ba53c04daaac842fdc01b85b35d843
Hakanasa <louisjonathan29@gmail.com>
2021-04-06 09:23:38 +0700

    .

--- app/src/main/java/umn/ac/id/tugasakhir_android/LoginActivity.java
+++ app/src/main/java/umn/ac/id/tugasakhir_android/LoginActivity.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -41,7 +42,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),ViewProfileActivity.class));
+                            startActivity(new Intent(getApplicationContext(),RestoHomeActivity.class));
                         }else {
                             Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
 
