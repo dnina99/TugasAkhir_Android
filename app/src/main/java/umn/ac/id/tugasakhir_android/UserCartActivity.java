2b095748ec6ad885dfcfc7a1ac27fac90a814fc9
chatyhuang <chaterine@astudent.umn.ac.id>
2021-04-24 22:34:45 +0700

    cart lanjutan

--- app/src/main/java/umn/ac/id/tugasakhir_android/UserCartActivity.java
+++ app/src/main/java/umn/ac/id/tugasakhir_android/UserCartActivity.java
@@ -1,14 +1,46 @@
 package umn.ac.id.tugasakhir_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import info.hoang8f.widget.FButton;
 
 public class UserCartActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseDatabase  database;
+    DatabaseReference request;
+
+    TextView txtTotalPrice;
+    FButton btnPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_cart);
+
+        database = FirebaseDatabase.getInstance();
+        request=database.getReference("Requests");
+
+        recyclerView = (RecyclerView)findViewById(R.id.listCart);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        txtTotalPrice = (TextView) findViewById(R.id.total);
+        btnPlace = (FButton) findViewById(R.id.btnPlaceOrder);
+    }
+
+    private void loadListFood(){
+
     }
 }
---
+++ app/src/main/java/umn/ac/id/tugasakhir_android/ViewHolder/CartAdapter.java
@@ -0,0 +1,20 @@
+package umn.ac.id.tugasakhir_android.ViewHolder;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+  public CartViewHolder(View itemView){
+    super(itemView);
+  }
+
+  @Override
+  public void onClick(View v) {
+
+  }
+}
+
+public class CartAdapter {
+}
