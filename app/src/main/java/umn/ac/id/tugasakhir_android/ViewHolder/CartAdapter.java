49b4ec76297e4450107da896fb593e03e781f481
Kleight <martinwongso234@gmail.com>
2021-05-18 22:02:04 +0700

    Cart

--- app/src/main/java/umn/ac/id/tugasakhir_android/ViewHolder/CartAdapter.java
+++ app/src/main/java/umn/ac/id/tugasakhir_android/ViewHolder/CartAdapter.java
@@ -84,7 +84,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartViewHolder> {
     TextDrawable drawable = TextDrawable.builder().buildRound("" + listData.get(position).getQuantity(), Color.RED);
     holder.img_cart_count.setImageDrawable(drawable);
 
-    Locale locale = new Locale("en", "US");
+    Locale locale = new Locale("in", "ID");
     android.icu.text.NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
     int price = (Integer.parseInt(listData.get(position).getPrice())) * (Integer.parseInt(listData.get(position).getQuantity()));
     holder.txt_price.setText(fmt.format(price));
