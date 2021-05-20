2b34ad05a31ca7791a58d50f1d464eecdfcbf30b
Hakanasa <louisjonathan29@gmail.com>
2021-05-20 20:44:35 +0700

    Update Delete food

--- app/src/main/java/umn/ac/id/tugasakhir_android/ViewHolder/FoodViewHolder.java
+++ app/src/main/java/umn/ac/id/tugasakhir_android/ViewHolder/FoodViewHolder.java
@@ -1,5 +1,6 @@
 package umn.ac.id.tugasakhir_android.ViewHolder;
 
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -7,10 +8,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import umn.ac.id.tugasakhir_android.Common.Common;
 import umn.ac.id.tugasakhir_android.Interface.ItemClickListener;
 import umn.ac.id.tugasakhir_android.R;
 
-public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener {
 
 
     public TextView food_name;
@@ -32,6 +34,7 @@ public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnCl
 
 
         itemView.setOnClickListener(this);
+        itemView.setOnCreateContextMenuListener(this);
 
 
     }
@@ -39,5 +42,13 @@ public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnCl
     @Override
     public void onClick(View v) {
         itemClickListener.onClick(v, getAdapterPosition(), false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+        contextMenu.setHeaderTitle("Pilih Aksi");
+
+        contextMenu.add(0,0,getAdapterPosition(), Common.UPDATE);
+        contextMenu.add(0,1,getAdapterPosition(), Common.DELETE);
     }
 }
