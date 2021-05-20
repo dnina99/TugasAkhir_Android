d3cc645384512998c3a798c58b942579a0e6b402
Kleight <martinwongso234@gmail.com>
2021-05-20 14:57:38 +0700

    FoodList

--- app/src/main/java/umn/ac/id/tugasakhir_android/FoodList.java
+++ app/src/main/java/umn/ac/id/tugasakhir_android/FoodList.java
@@ -70,7 +70,7 @@ public class FoodList extends AppCompatActivity {
 
     private void loadListFood(String categoryId){
         FirebaseRecyclerOptions<Food> options =
-                new FirebaseRecyclerOptions.Builder<Food>().setQuery(foodList.orderByChild("MenuId").equalTo(categoryId), Food.class).build();
+                new FirebaseRecyclerOptions.Builder<Food>().setQuery(foodList.orderByChild("menuId").equalTo(categoryId), Food.class).build();
 
         adapter = new FirebaseRecyclerAdapter<Food, FoodViewHolder>(options) {
             @Override
